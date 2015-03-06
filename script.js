f91cc058cb50e78dae59eb71c7f018f9afa78d78
NielsMeulders <niels.meulders@telenet.be>
2015-03-06 19:30:27 +0100

    added summary icons

--- script.js
+++ script.js
@@ -11,28 +11,40 @@
     }
     
     function success(position) {
-        y = position.coords.longitude;
-        x = position.coords.latitude;
-        var locatie = x + "," + y;
-        console.log(locatie);
-        var location_standard = "37.8267,-122.423";
-        var url = "https://api.forecast.io/forecast/7df4a8ead9a4572bf91f2a69224d9867/" + locatie;
         
-        $.ajax({
-            dataType: 'jsonp',
-            url: url,
-            success: function (result) {
-                
-                var temp = Math.round((Math.round(result.currently.temperature)-32) * 5/9);
-                
-                //localStorage.setItem("stations", JSON.stringify(stations));
-                
-                document.getElementById('temperature').innerHTML = temp;
-            },
-            error: function () {
-                
-            }
-        });
+            y = position.coords.longitude;
+            x = position.coords.latitude;
+            var locatie = x + "," + y;
+            console.log(locatie);
+            var location_standard = "37.8267,-122.423";
+            var url = "https://api.forecast.io/forecast/7df4a8ead9a4572bf91f2a69224d9867/" + locatie;
+
+            $.ajax({
+                dataType: 'jsonp',
+                url: url,
+                success: function (result) {
+
+
+                    var temp = Math.round((Math.round(result.currently.temperature)-32) * 5/9) + "°";
+                    var summ = result.currently.summary;
+                    
+                    switch (summ)
+                    {
+                        case 'Partly Cloudy': summ = 'C'
+                    }
+
+                    localStorage.setItem("res", JSON.stringify(temp));
+
+                    document.getElementById('temperature').innerHTML = temp;
+                    document.getElementById('summary').innerHTML = summ;
+                },
+                error: function () {
+
+                }
+            });
+        
+        
+        
     }
     
     function error() {
